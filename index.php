a387952ea452acf1fda35381dc6ca6a290b8e7e9
Adrià Muixí <adriamuixi@gmail.com>
2018-09-18 03:28:43 +0200

    Video background

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
 
 <body class="center-screen">
-<p class="color-red">Code, code, code....</p>
-
-<p class="color-gray">Minimal and clean</p>
-
-<p>
-    Contact to:
-    <a href="mailto:info@adriamuixi.com">info@adriamuixi.com</a></p>
+<div class="box-video">
+    <video autoplay muted loop id="background-video">
+        <source src="assets/videos/background_video.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+    </video>
+</div>
+<div class="box">
+    <p class="color-red first-paragraph">Code, code, code....</p>
+    <p class="color-gray second-paragraph">Minimal and clean</p>
+    <p class="third-paragraph">
+        Contact to:
+        <a href="mailto:info@adriamuixi.com">info@adriamuixi.com</a>
+    </p>
+</div>
 </html>
