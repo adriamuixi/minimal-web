48772e8a4b4894987b5812e02309542b93b1fec9
Adrià Muixí <adriamuixi@gmail.com>
2018-09-18 02:47:56 +0200

    styling web

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-<link href="style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body class="center-screen">
-<p style="color:#a84443">Code, code, code....</p>
+<p class="color-red">Code, code, code....</p>
 
-<p style="font-weight:bold">Minimal and clean</p>
+<p class="color-gray">Minimal and clean</p>
 
 <p>
-Contact to:
-<a href="mailto:info@adriamuixi.com">info@adriamuixi.com</a></p>
+    Contact to:
+    <a href="mailto:info@adriamuixi.com">info@adriamuixi.com</a></p>
 </html>
